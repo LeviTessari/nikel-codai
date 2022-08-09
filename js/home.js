eb8eda5f48798c5a524d7bdb6a99155a39e0adfc
Levi Tessari <levitessari@gmail.com>
2022-08-09 16:21:35 -0300

    att

--- js/home.js
+++ js/home.js
@@ -1,9 +1,33 @@
-const myModal = new bootstrap.Modal("#transection-modal");
+const myModal = new bootstrap.Modal("#transections-form");
 let logged = sessionStorage.getItem('logged');
 const session = localStorage.getItem("session");
 let data = {
-    tramsactions: []
+    transactions: []
 };
+
+document.getElementById("button-logout").addEventListener("click", logout);
+
+//Adicionar lançamento
+
+document.getElementById("transections-form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;
+    const date = document.getElementById("data-input").value;
+    const type = document.querySelector('input[nome="type-input"]:checked').value;
+
+    data.transactions.unshift({
+        value: value, type: type, description: description, date: date
+    });
+
+    saveData(data);
+    e.target.reset();
+
+    myModal.hide();
+    alert("Lançamento adicionado com sucesso.")
+
+})
 
 checkLogged();
 
@@ -20,4 +44,15 @@ function checkLogged(){
     if(dataUser){
         data= JSON.parse(dataUser);
     }
+    console.log(data);
+}
+
+function logout(){
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href= "index.html";
+}
+function saveData(data){
+    localStorage.setItem(data.login, JSON.stringify(data));
 }
