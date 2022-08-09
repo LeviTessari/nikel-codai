5f78a2f17922c92fa76a579e04b907674b37bfe7
Levi Tessari <levitessari@gmail.com>
2022-08-09 15:48:59 -0300

    att

---
+++ js/home.js
@@ -0,0 +1,23 @@
+const myModal = new bootstrap.Modal("#transection-modal");
+let logged = sessionStorage.getItem('logged');
+const session = localStorage.getItem("session");
+let data = {
+    tramsactions: []
+};
+
+checkLogged();
+
+function checkLogged(){
+    if(session){
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+    if(!logged){
+        window.location.href="index.html";
+        return;
+    }
+    const dataUser = localStorage.getItem(logged);
+    if(dataUser){
+        data= JSON.parse(dataUser);
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem('logged');
+const session = localStorage.getItem("session");
+
+checkLogged();
 //Logar no sistema
 
 document.getElementById("login-form").addEventListener("submit", function(e){
 e.preventDefault();
 const email = document.getElementById("email-input").value;
 const password = document.getElementById("password-input").value;
-const session = document.getElementById("session-check").checked;
+const checkSession = document.getElementById("session-check").checked;
 
 const account = getAccount(email);
 
@@ -18,10 +22,10 @@ const account = getAccount(email);
             alert("Opps! Verifique o usuário ou a senha.");
             return;
         }
+        saveSession(email, checkSession);
         window.location.href = "home.html"
-    }
-   
 
+    }    
 
 });
 
@@ -49,9 +53,33 @@ document.getElementById("create-form").addEventListener("submit", function(e){e.
     myModal.hide();
     alert("Conta criada com sucesso!");
 });
+
+function checkLogged(){
+    if(session){
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+    if(logged){
+        saveSession(logged, session);
+        window.location.href="home.html";
+    }
+
+}
+
+
 function saveAccount(data){
     localStorage.setItem(data.login, JSON.stringify(data));
 }
+
+function saveSession(data, saveSession){
+    if(saveSession){
+        localStorage.setItem("session", data);
+    }
+    sessionStorage.setItem("logged", data);
+
+}
+
+
 function getAccount(key){
     const account = localStorage.getItem(key);
     if(account){
@@ -59,3 +87,4 @@ function getAccount(key){
     }
     return "";
 }
+
