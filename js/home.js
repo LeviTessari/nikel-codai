a4eab31df3ccddfd90cec14b5aff4a2423c24eb4
Levi Tessari <levitessari@gmail.com>
2022-08-11 15:22:44 -0300

    att

--- js/home.js
+++ js/home.js
@@ -22,12 +22,16 @@ document.getElementById("transaction-form").addEventListener("submit", function(
     saveData(data);
     e.target.reset();
     myModal.hide()
+    
+    getCashIn();
+    getCashOut();
 
     alert("Lançamento adicionado com sucesso.");
-
+  
 });
 
 checkLogged();
+
 
 function checkLogged(){
     if(session){
@@ -42,7 +46,8 @@ function checkLogged(){
     if(dataUser){
         data = JSON.parse(dataUser);
     }
-    console.log(data)
+    getCashIn();
+    getCashOut();
     
 }
 
@@ -50,6 +55,93 @@ function logout(){
     sessionStorage.removeItem("logged");
     localStorage.removeItem("session");
     window.location.href= "index.html";
+}
+
+function getCashIn(){
+    const transactions = data.transactions;
+    
+    const cashIn = transactions.filter((item) => item.type == "1");
+
+    if(cashIn.length){
+        let cashInHtml= ``;
+        let limit = 0;
+
+        if(cashIn.length > 5){
+            limit = 5;
+        } else{
+            limit = cashIn.length;
+        }
+
+
+        for (let index = 0; index < limit; index++) {
+            cashInHtml += `
+                                    <div class="row mb-4">
+                                        <div class="col-12">
+                                            <h3 class="fs-2">R$ ${cashIn[index].value.toFixed(2)}</h3>
+                                            <div class="container p-0">
+                                                <div class="row">
+                                                    <div class="col-12 col-md-8"> 
+                                                        <p>${cashIn[index].description}</p>
+                                                    </div>
+                                                    <div class="col-12 col-md-3 d-flex justify-content-end">
+                                                        ${cashIn[index].date}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    `
+            
+        }
+        document.getElementById("cash-in-list").innerHTML = cashInHtml
+
+    }
+
+  
+    
+}
+function getCashOut(){
+    const transactions = data.transactions;
+    
+    const cashIn = transactions.filter((item) => item.type == "2");
+
+    if(cashIn.length){
+        let cashInHtml= ``;
+        let limit = 0;
+
+        if(cashIn.length > 5){
+            limit = 5;
+        } else{
+            limit = cashIn.length;
+        }
+
+
+        for (let index = 0; index < limit; index++) {
+            cashInHtml += `
+                                    <div class="row mb-4">
+                                        <div class="col-12">
+                                            <h3 class="fs-2">R$ ${cashIn[index].value.toFixed(2)}</h3>
+                                            <div class="container p-0">
+                                                <div class="row">
+                                                    <div class="col-12 col-md-8"> 
+                                                        <p>${cashIn[index].description}</p>
+                                                    </div>
+                                                    <div class="col-12 col-md-3 d-flex justify-content-end">
+                                                        ${cashIn[index].date}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    `
+            
+        }
+        document.getElementById("cash-out-list").innerHTML = cashInHtml
+
+    }
+
+  
+    
 }
 
 function saveData(data){
